1dff7b47e567ad62f63db60311d5d5093569b78d
fangbaooo <66042773@qq.com>
2017-09-21 23:18:06 +0800

    add new

---
+++ src/page/appdl/appdl.vue
@@ -0,0 +1,65 @@
+<template>
+	<div>
+		<div class="dl_top">
+			<div class="s_center">
+				<div class="dl_top_con">
+					<img src="../../assets/images/erweima/app.jpg" alt="" class="app_ma">
+					<div class="dl_btn">
+						<a href="https://itunes.apple.com/cn/app/yi-tian-li-cai/id1094562960?mt=8" target="_blank"><i class="icon_iphone"></i>iPhone版下载</a>
+						<a href="https://www.yitianlicai.com/apk"><i class="icon_android" target="_blank"></i>安卓版下载</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="dl_center">
+			<ul class="s_center dl_con clearfix">
+				<li><i class="dl_func_icon dl_func_icon1"></i><p class="fz16">更快捷</p><span class="fz14">随时随地掌握投资动态</span></li>
+				<li><i class="dl_func_icon dl_func_icon2"></i><p class="fz16">更人性</p><span class="fz14">你的理财小助手</span></li>
+				<li><i class="dl_func_icon dl_func_icon3"></i><p class="fz16">更放心</p><span class="fz14">抓住每一次优惠，省得更多，赚得更多</span></li>
+			</ul>
+			<div class="dl_con_ad"><img src="../../assets/images/appDownload/dl_center.jpg" alt=""></div>
+		</div>
+		<div class="dl_bottom">
+			<div class="s_center">
+				<p class="dl_bottom_sao">扫一扫二维码下载</p>
+				<div class="dl_bottom_con">
+					<img src="../../assets/images/erweima/app.jpg" alt="" class="app_ma">
+					<div class="dl_btn">
+						<a href="https://itunes.apple.com/cn/app/yi-tian-li-cai/id1094562960?mt=8" target="_blank"><i class="icon_iphone"></i>iPhone版下载</a>
+						<a href="https://www.yitianlicai.com/apk"><i class="icon_android" target="_blank"></i>安卓版下载</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<style>
+.dl_top{ height: 550px; background: #4896da url("../../assets/images/appDownload/dl_top.jpg") center top;}
+.dl_top_con{ padding: 298px 0 0 505px}
+.app_ma{ float: left; background-color: #fff; width: 144px; height: 144px; padding: 2px;}
+.dl_btn{ float: left; width: 200px; margin-left: 20px;}
+.dl_btn a{ display: block; height: 60px; padding-left: 16px; margin-bottom: 20px; border: 2px solid #fff; line-height: 60px; color: #fff; font-size: 18px; border-radius: 2px}
+.dl_btn a:hover{ border-color: #ea5404; background-color: #ea5404;}
+.dl_btn i{ float: left; width: 36px; height: 36px; margin: 12px 5px 12px 0;background: url("../../assets/images/appDownload/dl_app_s.png");}
+.dl_btn i.icon_android{background-position: right top;}
+
+.dl_center{ background-color: #f4f4f7;}
+.dl_con { padding-top: 70px;}
+.dl_con li{ float: left; width: 330px; text-align: center; line-height: 24px;}
+.dl_con i{ display: block; width: 106px; height: 106px; margin: 0 auto; margin-bottom: 5px;}
+.dl_con i.dl_func_icon{background: url("../../assets/images/appDownload/dl_icon.jpg");}
+.dl_con i.dl_func_icon2{ background-position: -106px 0;}
+.dl_con i.dl_func_icon3{ background-position: -212px 0;}
+.dl_con p{ line-height: 24px;}
+.dl_con_ad{ padding: 55px 0 45px; width: 874px; margin: 0 auto}
+
+.dl_bottom{ height: 549px; background: #aaaaaa url("../../assets/images/appDownload/dl_bottom.jpg") center top;}
+.dl_bottom_sao{ padding-top: 90px; font-size: 30px; color: #fff; text-align: center;}
+.dl_bottom_con{ width: 630px; margin: 50px auto 0;}
+.dl_bottom_con .app_ma{ float: left; background-color: #fff; width: 240px; height: 240px; padding: 7px;}
+.dl_bottom_con .dl_btn{ float: left; width: 342px; margin-left: 34px;}
+.dl_bottom_con .dl_btn a{ display: block; height: 106px; padding-left: 22px; margin-bottom: 34px; border: 2px solid #fff; line-height: 106px; color: #fff; font-size: 30px; border-radius: 2px}
+.dl_bottom_con .dl_btn a:hover{ border-color: #ea5404; background-color: #ea5404;}
+.dl_bottom_con .dl_btn i{ float: left; width: 60px; height: 60px; margin: 23px 12px 23px 0;background: url("../../assets/images/appDownload/dl_app.png"); }
+.dl_bottom_con .dl_btn i.icon_android{background-position: right top;}
+</style>
