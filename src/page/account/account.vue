5a5f7d55ab6854efd5f2a30113efd5cc10434b2b
fangbaooo <66042773@qq.com>
2017-09-28 16:15:52 +0800

    modify

--- src/page/account/account.vue
+++ src/page/account/account.vue
@@ -13,8 +13,10 @@
           <router-link to="/account/home" @click.native="navEvent('账户总览')" active-class="selected"><h4> <i class="icon ico1"></i>我的账户</h4></router-link>
           <ul class="p_zhminav">
             <router-link to="/account/home" tag="li" @click.native="navEvent('账户总览')" active-class="clicked" exact><a>账户总览</a></router-link>
-            <li @click="doCheck(1, $event)" id="recharge"><a>充值</a></li>
-            <li @click="doCheck(2, $event)" id="withdrawal"><a>提现</a></li>
+            <router-link to="/account/home/recharge" tag="li" @click.native="navEvent('充值')" active-class="clicked" exact><a>充值</a></router-link>
+            <router-link to="/account/home/withdrawal" tag="li" @click.native="navEvent('提现')" active-class="clicked" exact><a>提现</a></router-link>
+            <!-- <li @click="doCheck(1, $event)" id="recharge"><a>充值</a></li>
+            <li @click="doCheck(2, $event)" id="withdrawal"><a>提现</a></li> -->
             <router-link to="/account/home/dealRecord" id="dealRecord" @click.native="navEvent('交易记录')" tag="li" active-class="clicked" exact><a>交易记录</a></router-link>
           </ul>
         </li>
@@ -62,19 +64,27 @@ export default {
   data () {
     return {
       breadcrumbs: '账户总览',
-      link: [
-        { type: "recharge", text: "充值"},
-        { type: "withdrawal", text: "提现"},
-        { type: "dealRecord", text: "交易记录"}
-      ]
+      // link: [
+      //   { type: "recharge", text: "充值"},
+      //   { type: "withdrawal", text: "提现"},
+      //   { type: "dealRecord", text: "交易记录"}
+      // ]
     }
   },
   mounted () {
-    this.setRouter();
+    //this.setRouter();
   },
-  watch: {
-    "$route": "setRouter"
+  beforeRouteUpdate(to, from, next){
+    // 跳转到充值前的判断，暂时未添加任何判断
+    if (to.path.indexOf("home/recharge") > -1) {
+      next()
+    } else {
+      next()
+    }
   },
+  // watch: {
+  //   "$route": "setRouter"
+  // },
   methods: {
     // 设置面包导航文字
     setBreadcrumbs (name) {
@@ -83,7 +93,7 @@ export default {
     // 绑定导航点击事件
     navEvent (name) {
       this.setBreadcrumbs(name);
-      this.removeRechargeNav();
+      //this.removeRechargeNav();
     },
     // 去掉充值和提现的样式
     removeRechargeNav () {
@@ -95,32 +105,36 @@ export default {
       }
     },
     // 判断当前是否充值和提现页面
-    setRouter () {
-      var link = this.link
-      for (var i = 0; i < link.length; i++) {
-        if (this.$route.path.indexOf(link[i].type) > -1) {
-          this.setBreadcrumbs(this.link[i].text)
-          document.getElementById(link[i].type).className = 'clicked'
-        } else {
-          document.getElementById(link[i].type).className = ''
-        }
-      }
-    },
+    // setRouter () {
+    //   var link = this.link;
+    //   for (var i = 0; i < link.length; i++) {
+    //     if (this.$route.path.indexOf(link[i].type) > -1) {
+    //       this.setBreadcrumbs(this.link[i].text);
+    //       if(document.getElementById(link[i].type)){
+    //         document.getElementById(link[i].type).className = 'clicked'
+    //       }
+    //     } else {
+    //       if(document.getElementById(link[i].type)){
+    //         document.getElementById(link[i].type).className = ''
+    //       }
+    //     }
+    //   }
+    // },
     // 充值和提现前的判断
-    doCheck(type, e) {
-      if (type === 1) {
-        this.setBreadcrumbs("充值")
-        e.currentTarget.className = "clicked";
-        this.$router.push({ path: '/account/home/recharge'})
-        this.removeRechargeNav()
-      }
-      if (type === 2) {
-        this.setBreadcrumbs("提现")
-        e.currentTarget.className = "clicked";
-        this.$router.push({ path: '/account/home/withdrawal'})
-        this.removeRechargeNav()
-      }
-    }
+    // doCheck(type, e) {
+    //   if (type === 1) {
+    //     this.setBreadcrumbs("充值")
+    //     e.currentTarget.className = "clicked";
+    //     this.$router.push({ path: '/account/home/recharge'})
+    //     this.removeRechargeNav()
+    //   }
+    //   if (type === 2) {
+    //     this.setBreadcrumbs("提现")
+    //     e.currentTarget.className = "clicked";
+    //     this.$router.push({ path: '/account/home/withdrawal'})
+    //     this.removeRechargeNav()
+    //   }
+    // }
   }
 }
 </script>
