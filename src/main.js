d7a2bdb41376075a77b0cc476b38609bfad2811c
fangbaooo <66042773@qq.com>
2017-09-29 15:38:26 +0800

    modify

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import Vuex from 'vuex'
 import axios from 'axios'
 import App from './App'
 import router from './router'
+import '@/assets/css/public.css'
+import '../static/js/plugins/artDialog/dialog-plus-min.js'
 import store from './store'
 import direcive from './directive'
 import './utils/permission' // 权限
